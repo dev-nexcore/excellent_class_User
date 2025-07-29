72c702d1d4595fb58339d5a8d2e320d9fb8a30a4
juned187 <junedsayyed381@gmail.com>
2025-07-29 19:01:56 +0530

    home-2section-complete

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,12 +2,17 @@ import Footer from "@/components/common/Footer";
 import Navbar from "@/components/common/Navbar";
 import ContactUs from "@/components/home/ContactUs";
 import ContactCards from "@/components/home/ContactUs";
+import ExamSection from "@/components/home/stream2";
+import StreamAndCourses from "@/components/home/streamandcourses";
+import WhyChooseUs from "@/components/home/whychooseus";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      
+      <WhyChooseUs/>
+      <StreamAndCourses/>
+      <ExamSection/>
     </div>
   );
 }
