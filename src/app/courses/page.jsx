ec4a2db8099d258ee579da53e8371db799bef0af
Atharva252 <atharvabhintade@gmail.com>
2025-07-30 17:02:30 +0530

    text-change-in-cources

---
+++ src/app/courses/page.jsx
@@ -0,0 +1,10 @@
+import CourcesHomePage from "@/components/Courses/home page";
+
+export default function Home (){
+    return (
+        <>
+       <CourcesHomePage/>
+        
+        </>
+    )
+}
