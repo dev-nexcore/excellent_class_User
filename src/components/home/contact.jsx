e860c61f5088166dcafd25d4139f1b494c1c2c8e
sabina <shaikhsabina637@gmail.com>
2025-08-12 12:13:33 +0530

    final submission by sabina

--- src/components/home/contact.jsx
+++ src/components/home/contact.jsx
@@ -44,36 +44,30 @@ export default function ContactUs() {
         </motion.div>
 
         {/* Head Office */}
-        <motion.div
-          className="w-full min-w-[300px] max-w-[400px] h-[460px] p-6 rounded-xl bg-[#FFA629] text-black shadow-lg flex flex-col items-center text-center hover:-translate-y-[5px] transition-transform duration-300 md:w-[calc(50%-1rem)] lg:w-[calc(33.333%-2rem)]"
-          initial={{ opacity: 0, y: -100 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.2, ease: "linear" }}
-          viewport={{ once: true, amount: 0.1 }}
-        >
-          <h3 className="text-3xl md:text-[30px] font-bold mb-4 pb-2 border-b-2 border-current w-[80%]">
-            Head Office
-          </h3>
-          <p className="text-lg md:text-[20px] leading-[1.7] font-normal">
-            1, VIP Villa, House No. 16, Next to Michael High School, Kurla (W),
-            Mumbai – 400 070.
-          </p>
-          <div className="flex items-center gap-6 mt-8 justify-start w-full">
-            <FaPhoneAlt className="text-xl" />
-            <div className="text-left">
-              <p className="text-lg md:text-[20px]">022 - 25039452</p>
-              <p className="text-lg md:text-[20px]">9892755100</p>
-              <p className="text-lg md:text-[20px]">9136955101</p>
-            </div>
+       <motion.div
+        className="w-full min-w-[300px] max-w-[400px] h-[460px] p-6 rounded-xl bg-[#FFA629] text-black shadow-lg flex flex-col items-center text-center hover:-translate-y-[5px] transition-transform duration-300 md:w-[calc(50%-1rem)] lg:w-[calc(33.333%-2rem)]"
+        initial={{ opacity: 0, y: -100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.2, ease: "linear" }}
+        viewport={{ once: true, amount: 0.1 }}
+      >
+        <h3 className="text-3xl md:text-[30px] font-bold mb-4 pb-2 border-b-2 border-current w-[80%]">Head Office</h3>
+        <p className="text-lg md:text-[20px] leading-[1.7] font-normal">
+          1, VIP Villa, House No. 16, Next to Michael High School, Kurla (W), Mumbai – 400 070.
+        </p>
+        <div className="flex items-center gap-6 mt-8 justify-start w-full">
+          <FaPhoneAlt className="text-xl" />
+          <div className="text-left">
+            <p className="text-lg md:text-[20px]">022 - 25039452</p>
+            <p className="text-lg md:text-[20px]">9892755100</p>
+            <p className="text-lg md:text-[20px]">9136955101</p>
           </div>
-          <div className="flex items-center gap-6 mt-2 justify-start w-full">
-            <FaEnvelope className="text-[24px] md:text-[3vw] lg:text-[64px]" />
-
-             <p className="text-sm md:text-[20px]">info@excellentclasses.in</p>
-</div>
-
-        </motion.div>
-
+        </div>
+        <div className="flex items-center gap-3 mt-2 py-3 justify-start w-full">
+          <FaEnvelope className="text-3xl" />
+          <p className="text-sm md:text-[20px]">info@excellentclasses.in</p>
+        </div>
+      </motion.div>
         {/* Last Branch Office */}
         <motion.div
           className="w-full min-w-[300px] max-w-[400px] h-[460px] p-6 rounded-xl bg-[#20356B] text-white shadow-lg flex flex-col items-center text-center hover:-translate-y-[5px] transition-transform duration-300 md:w-[calc(50%-1rem)] lg:w-[calc(33.333%-2rem)]"
@@ -173,7 +167,7 @@ export default function ContactUs() {
       </div>
 
       {/* Google Maps */}
-      <div className="flex justify-center p-5 bg-gray-50">
+      <div className="flex justify-center p-14 bg-gray-50">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3769.8650541329965!2d72.88528901490558!3d19.11707385579073!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c8f2d9d7e4df%3A0x3e0cfa04db26a7db!2sExcellent%20Classes!5e0!3m2!1sen!2sin!4v1653489517022!5m2!1sen!2sin"
           className="w-full md:w-[90%] lg:w-[75%] h-[350px] rounded-xl border-0 shadow-lg"
